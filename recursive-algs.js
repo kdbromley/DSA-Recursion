ea905def646b2979a79c20b5a89363798f23515f
k.d. Bromley <73963292+kdbromley@users.noreply.github.com>
2021-06-18 18:39:16 -0700

    Update recursive-algs.js

--- recursive-algs.js
+++ recursive-algs.js
@@ -1,27 +1,49 @@
-const countSheep = function(sheep) {
+function countSheep(sheep) {
+  //Base Case
   if(sheep === 0) {
-    return 'All sheep have jumped over the fence';
+    console.log('All sheep have jumped over the fence');
   };
-  return (countSheep((sheep-1)) + ': ' + 'Another sheep jumps over the fence');
+  //Recursive Case
+  console.log(sheep + ': Another sheep jumps over the fence');
+  return countSheep(sheep - 1)
 }
 
-let sheep = 3;
+countSheep(3)
 
 
-const powerCalculator =  function(base, exp) {
-  if(exp < 0) {
-    return 'Exponent should be >= 0';
-  };
+function powerCalculator(b, e) {
+  //Error case
+  if(e<0) return 'e must be >=0';
+  if(e === 0) return 1;
+  //Base case
+  if(e === 1) return b;
+  //Recursive case
+  return b * powerCalculator(b, (e - 1))
 }
 
-let base = 10;
-let exp = 2;
+console.log(powerCalculator(10,2))
 
 
-const reverseString = function(string) {
-  let res = string.split("");
-  res.length
+function reverse(string) {
+  //Base case
+  if(string.length < 2) return string;
+  //Recursive case
+  return reverse(string.slice(1, string.length)) + string[0]
 }
 
-let string = 'func';
+console.log(reverse('dog'));
+
+
+function triangularNum(n) {
+  //Error case
+  if(n<1) return 0;
+  //Base case
+  if(n === 1) return n;
+  //Recursive case
+  return n + triangularNum(n - 1)
+}
+
+console.log(triangularNum(5))
+
+
   
